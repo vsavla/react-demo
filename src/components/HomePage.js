01d5e87d68ade10285e4bb770fa7186c4bfb0763
Vaibhav Savla <vaibhav@Vaibhavs-MacBook-Pro.local>
2020-01-02 13:12:42 +0530

    removed bootstrap dependency

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const jumbotron = {
+    padding: "4rem 2rem",
+    marginBottom: "2rem",
+    backgroundColor: "#e9ecef",
+    borderRadius: ".3rem"
+  };
+
   return (
-    <div className="jumbotron">
+    <div style={jumbotron}>
       <h1>Pluralsight Administration</h1>
       <p>React, Flux and React Router or responsive web sites</p>
       <Link to="about" className="btn btn-primary">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
 render(
   <Router>
